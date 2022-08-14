0ee552bc2d61e56597b6afd76928329b2cf3952d
Javier Uncos <javiersrfc30@gmail.com>
2022-08-14 02:23:25 -0300

    ejercicio 4 realizado

--- js/app.js
+++ js/app.js
@@ -19,48 +19,96 @@ function reloj(){
     let fecha = document.getElementById("fecha");
     let fecha2 = new Date();
     let fecha3 = fecha2.getDate();
-    fecha.innerHTML =  fecha3 + "/";
+    fecha.innerHTML =  fecha3 + "  ";
     // dias de la semana
     let diaDesemana = document.getElementById("diaSemana");
     let diaDesemana2 = new Date();
     let diaSemana3 = diaDesemana2.getDay();
-    // if(diaSemana3 === 6){
-    //    diaDesemana.innerHTML= "viernes";
-    // }
     switch(true){
         case diaSemana3 === 1:
-            diaDesemana.innerHTML= "lunes";
+            diaDesemana.innerHTML= "lunes ";
             break
         case diaSemana3 === 2:
-            diaDesemana.innerHTML= "martes";
+            diaDesemana.innerHTML= "martes ";
             break
         case diaSemana3 === 3:
-            diaDesemana.innerHTML= "miercoles";
+            diaDesemana.innerHTML= "miercoles ";
             break
         case diaSemana3 === 4:
-            diaDesemana.innerHTML= "jueves";
+            diaDesemana.innerHTML= "jueves ";
             break
         case diaSemana3 === 5:
-            diaDesemana.innerHTML= "viernes";
+            diaDesemana.innerHTML= "viernes ";
             break
         case diaSemana3 === 6:
-            diaDesemana.innerHTML= "sabado";
+            diaDesemana.innerHTML= "sabado ";
             break
-        case diaSemana3 === 7:
-            diaDesemana.innerHTML= "domingo";
+        case diaSemana3 === 0:
+            diaDesemana.innerHTML= "domingo ";
             break
 
     }
-    // diaDesemana.innerHTML= diaSemana3;
+
+    // codigo para mostrar los meses
+     let meses = document.getElementById("meses");
+     let meses2 = new Date();
+     let meses3 = meses2.getMonth();
+    //  meses.innerHTML = meses3;
+    switch(true){
+        case meses3 === 0 :
+            meses.innerHTML = "Enero";
+            break
+        case meses3 === 1 :
+            meses.innerHTML = "Febrero";
+            break
+        case meses3 === 2 :
+            meses.innerHTML = "Marzo";
+            break
+        case meses3 === 3 :
+            meses.innerHTML = "Abril ";
+            break
+        case meses3 === 4 :
+            meses.innerHTML = "Mayo ";
+            break
+        case meses3 === 5 :
+            meses.innerHTML = "Junio ";
+            break
+        case meses3 === 6 :
+            meses.innerHTML = "Julio ";
+            break
+        case meses3 === 7 :
+            meses.innerHTML = "Agosto ";
+            break
+        case meses3 === 8 :
+            meses.innerHTML = "Septiembre ";
+            break 
+        case meses3 === 9 :
+            meses.innerHTML = "Octubre ";
+            break
+        case meses3 === 10 :
+            meses.innerHTML = "Noviembre ";
+            break
+        case meses3 === 11 :
+            meses.innerHTML = "Diciembre ";
+            break
+    }
+
+    // mostrar el año
+    let anios = document.getElementById("años");
+    let anios2 = new Date();
+    let anios3 = anios2.getFullYear()
+    anios.innerHTML= anios3 ;
 
     
 
 
-     
-    console.log(diaSemana3)
+    
     
 }
 
 
 reloj()
 setInterval(reloj,1000)
+
+
+/* decidi no mostrar el reloj en dispositivos celulares por cuestion de estetica*/
